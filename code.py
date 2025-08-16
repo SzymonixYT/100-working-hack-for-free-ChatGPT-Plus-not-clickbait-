454a141f2d8f3da241b1e8568c38c09deecc174f
szmonix <102387888+SzymonixYT@users.noreply.github.com>
2025-08-16 14:19:35 +0200

    Update code.py

--- code.py
+++ code.py
@@ -8,15 +8,15 @@ helloworld("hacking open ai to get free chatgpt plus...")
 Tru = "True"
 answer = input("first milk or cereal ")
 while Tru == Tru:
-progress = 0
-if answer == "cereal":
-while Tru == Tru:
-ran = random.randint(1, 10000)
-progress = ran + progress
-helloworld(f"progress: {progress}%")
-time.sleep(1)
-else:
-system32 = "gone"
-print("system32 is " + system32)
-break
+    progress = 0
+    if answer == "cereal":
+        while Tru == Tru:
+            ran = random.randint(1, 10000)
+            progress = ran + progress
+            helloworld(f"progress: {progress}%")
+            time.sleep(1)
+    else:
+    system32 = "gone"
+    print("system32 is " + system32)
+    break
 
