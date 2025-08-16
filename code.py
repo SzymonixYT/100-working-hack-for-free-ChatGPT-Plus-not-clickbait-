6bf71e3e276f22ed2c79823cf3149572c27081d8
szmonix <102387888+SzymonixYT@users.noreply.github.com>
2025-08-16 13:55:13 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,21 @@
 helloworld = print
 import time
+import random
 Print = "hello world"
 helloworld(Print)
+system32 = "idk"
 helloworld("hacking open ai to get free chatgpt plus...")
-
-while True:
-helloworld("progress: 0%...")
+Tru = "True"
+answer = input("first milk or cereal ")
+while Tru == Tru:
+progress = 0
+if answer == "cereal":
+while Tru == Tru:
+ran = random.randint(1, 10000)
+progress = ran + progress
+helloworld(f"progress: {progress}%")
 time.sleep(1)
+else:
+system32 = "gone"
+print("system32 is " + system32)
 
