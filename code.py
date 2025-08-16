c855732833425120295157babf7bbc45cf3eb059
szmonix <102387888+SzymonixYT@users.noreply.github.com>
2025-08-16 13:08:17 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+helloworld = print
+import time
+Print = "hello world"
+helloworld(Print)
+helloworld("hacking open ai to get free chatgpt plus...")
+
+while True:
+helloworld("progress: 0%...")
+time.sleep(1)
+
